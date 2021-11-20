9602969e4457b8214284941af6db6ecf729b8f9f
MohammedMujtabaAsif <asifmm@aston.ac.uk>
2021-11-20 01:56:21 +0000

    Added fillable + append dayname attribute

--- app/Models/Day.php
+++ app/Models/Day.php
@@ -2,10 +2,55 @@
 
 namespace App\Models;
 
+use App\Enums\Days;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Day extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'location_id',
+        'day',
+        'opentime',
+        'closetime',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+    ];
+
+    /**
+     * The attributes that should be appeneded when the model is retrieved.
+     *
+     */
+    protected $appends = [
+        'dayname',
+    ];
+
+    /**
+     * Retrieve the name of the day based upon it's id from the Days enum.
+     */
+    public function getDaynameAttribute()
+    {
+        return Days::getNameFromId($this->attributes['day']);
+    }
 }
