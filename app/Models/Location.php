73d2bd74fcb840bcef01334e463c397073d267a8
MohammedMujtabaAsif <asifmm@aston.ac.uk>
2021-11-20 01:57:12 +0000

    Added fillable and HasMany relation to Days model

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -8,4 +8,39 @@ use Illuminate\Database\Eloquent\Model;
 class Location extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'postcode',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+    ];
+
+    /**
+    * Get all of the days for the Location
+    *
+    * @return \Illuminate\Database\Eloquent\Relations\HasMany
+    */
+    public function days()
+    {
+        return $this->hasMany(Day::class);
+    }
 }
