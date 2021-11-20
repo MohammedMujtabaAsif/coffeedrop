664d8e1b220cb2f933f728364ce20f0833872648
MohammedMujtabaAsif <asifmm@aston.ac.uk>
2021-11-20 02:01:17 +0000

    File reader and seeder for Locations+Days tables

--- database/seeders/LocationSeeder.php
+++ database/seeders/LocationSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Day;
+use App\Models\Location;
 use Illuminate\Database\Seeder;
 
 class LocationSeeder extends Seeder
@@ -13,6 +15,51 @@ class LocationSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Empty the databases
+        Location::truncate();
+        Day::truncate();
+//
+        // Open the file that will be read
+        $dataFile = fopen(base_path('/database/data/location_data.csv'), 'r');
+
+        // Track first line of titles in the csv file
+        $isFirstLine = true;
+
+        // Continue to loop through the csv file until it reaches the end of the file
+        while (($data = fgetcsv($dataFile, 2000, ',')) !== false) {
+
+            // Check the $data is not the titles line
+            if (!$isFirstLine) {
+
+                // Store a Location model containing the postcode of the CoffeeDrop location
+                $location = Location::create(['postcode' => $data[0]]);
+
+                // Tracks the number of days that have been saved
+                $dayCount = 1;
+
+                // Loop through the day creation for the number of days in a week
+                while ($dayCount <= 7) {
+                    // Retrive the open and close time for this day of the week
+                    $opentime = $data[$dayCount];
+                    $closetime = $data[$dayCount + 7];
+
+                    // Store a Day model
+                    Day::create([
+                        // Use the location id from the Location model created above
+                        'location_id' => $location->id,
+                        // Store the day's number in the week
+                        'day' => $dayCount,
+
+                        // Store the retrieved time values; store null if time not provided
+                        'opentime' => $opentime != '' ?? $opentime,
+                        'closetime' => $closetime != '' ?? $closetime,
+                    ]);
+
+                    // Increment the counter to store the next day
+                    $dayCount++;
+                }
+            }
+            $isFirstLine = false;
+        }
     }
 }
