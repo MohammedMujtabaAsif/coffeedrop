06633951855cb53fc9623225a531f2a568fbe1d1
MohammedMujtabaAsif <asifmm@aston.ac.uk>
2021-11-21 21:24:12 +0000

    Moved into Api Folder

---
+++ app/Http/Controllers/Api/LocationController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Location;
+use App\Traits\ResponseTrait;
+use Illuminate\Http\Request;
+
+class LocationController extends Controller
+{
+    use ResponseTrait;
+
+}
